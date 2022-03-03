b20ad47be7b8843360adaa0eb9a45a12795904f1
Peter <006815296@coyote.csusb.edu>
2022-03-03 12:35:57 -0800

    CHANGES

---
+++ resources/views/board.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'board')
+
+@section('content_header')
+    <h1>board</h1>
+@stop
+
+@section('content')
+    <p>Content here</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
